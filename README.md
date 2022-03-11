45009275edb350c83f78558032a3702f640a58c9
Fanjasoa ANDRIAMANJAKA <hei.fanjasoa@gmail.com>
2022-03-11 10:09:59 +0100

    feat: add all project file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Exam
+## description
+this is my _web exam_
+````git
+git clone https://github.com/fanjasoa18/exam.git
+````
---
+++ index.php
@@ -0,0 +1,398 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="fr">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Floix - Onepage Resume Bootstrap Template</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<!-- Header Section -->
+<section class="banner" role="banner"> 
+  <!-- Navigation Section -->
+  <header id="header">
+    <div class="header-content clearfix"> <a class="logo" href="#"> <img src="../floix_template/img-prof/logo.gif" alt=""></a>
+      <nav class="navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#introduction">Accueil</a></li>
+          <li><a href="#works">Formation</a></li>
+          <li><a href="#concours">Concours</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+  </header>
+  <!-- Navigation Section --> 
+  <!-- Banner Section -->
+  <div class="container">
+    <div class="col-md-7 banner-inner-wrapper">
+      <div class="banner-text">
+        <h1>Haute Ecole d'Informatique</h1>
+        <p>Professionnels du monde entier unis pour former les étudiants malgaches suivant les plus hauts standarts.</p>
+        <a href="#works" class="clic"><b>Plus d'info</b></a> </div>
+      <!-- banner text --> 
+    </div>
+  </div>
+  <!-- Banner Section --> 
+</section>
+<!-- Header Section --> 
+<!-- Intro Section -->
+<section id="introduction" class="section introduction">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="intro-content">
+          <h1>Qui Sommes - Nous ?</h1>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="intro-content">
+          <p><b>HEI</b> est une école supérieure ayant pour ambition de proposer une offre de formation en adéquation avec les attentes du marché de travail actuel et des mutations digitales de la société.</p>
+          <p><b>HEI</b> est forte d'une équipe d'enseignants spécialisés et de partenaires d'insertion professionnels locaux et internationaux. <b>HEI</b> est composé d'enseignants dont la majeur partie est titulaire d'un doctorat en informatique et de cadres professionnels en entreprise.</p>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-6">
+        <div class="intro-content">
+          <h4>Pourquoi choisir <b class="hei">HEI</b> ?</h4>
+          <p>Le choix de <b>HEI</b> serait motivé avant tout par :</p>
+          <ul>
+            <li> . La qualité de l'enseignant assurée par des professionnels de haut niveau</li>
+            <li> . La méthodologie de la formation qui intègre le développement personnel des jeunes étudiants et la culture de résultat d'un futur manager et leader international</li>
+            <li> . La garantie du stage et/ou du contrat d'alternance en entreprise, grâce aux partenaires d'<b>HEI</b>.</li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="contenu">
+    <div class="row">
+      <div class="col-md-4 col-sm-4">
+        <img src="../floix_template/img-prof/entraide.jpg" alt="">
+        <h5>ENTRAIDE</h5>
+        <p>Nous sommes animés par l'état d'esprit du "valimbabena" par des intellectuels malagasy, formés dans les meilleures universités locales et internatinales. Ces derniers souhaitent partager leur savoir et leur expérience à la jeune génération.</p>
+
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <img src="../floix_template/img-prof/performance.jpg" alt="">
+        <h5>PERFORMANCE</h5>
+        <p>Nous recherchons continuellement l'efficacité maximum et cultivons la culture de résultat. Chaque acteur d'<b>HEI</b> est ainsi responsable et recherche toujours la meilleure façon d'atteindre les objectifs fixés.</p>
+
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <img src="../floix_template/img-prof/autonomie.jpg" alt="">
+        <h5>AUTONOMIE</h5>
+        <p>Nous favorisons une culture d'autonomie qui vise à responsabiliser chaque étudiant par la prise en main de son avenir dès sa vie estudiantine et par le développement progressif de son professionnalisme.</p>
+
+      </div>
+
+    </div>
+
+  </div>
+  <div class="about">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <h3>Une petite présentation en malgache à propos de <b class="hei">HEI</b>, faite par le fondateur F. Lou MAURICA.</h3>
+
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <video src="img-prof/Presentation.mp4" controls></video>
+      </div>
+      
+    </div>
+  </div>
+</section>
+
+
+<!-- Intro Section --> 
+<section id="works" class="works section no-padding">
+  <h3>Un aperçu de l'équipe pédagogique</h3>
+  <div class="contain">
+    <div class="row">
+      <div class="col-md-3 col-sm-3">
+        <img src="../floix_template/img-prof/Ryan.jpg" alt="">
+        <h5>RYAN</h5>
+        <p>Responsable administratif et Technique. Ensaignant du module Théorie1</p>
+
+      </div>
+      <div class="col-md-3 col-sm-3">
+        <div class="two">
+          <img src="../floix_template/img-prof/Tahina.PNG" alt="">
+          <h5>TAHINA</h5>
+          <p>Docteure-Ingénieure CEA, Saclay.
+            Laureate du prix UNESCO-l'Oréal pour les femmes en science, 2017
+          </p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-3">
+        <div class="three">
+          <img src="../floix_template/img-prof/Toky.jpg" alt="">
+          <h5>TOKY</h5>
+          <p>enseignant HEI & EMIT,
+            Doctorant en systèmes Multi-Agents
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="contain2">
+    <div class="row">
+      <div class="col-md-3 col-sm-3">
+        <img src="../floix_template/img-prof/Parison.PNG" alt="">
+        <h5>PARISON</h5>
+        <p>Ingenieur GOOGLE, Londres.</p>
+
+      </div>
+      <div class="col-md-3 col-sm-3">
+        <div class="four">
+          <img src="../floix_template/img-prof/Sandrine.PNG" alt="">
+          <h5>SANDRINE</h5>
+          <p>Précedente directrice technique de Vanilla Pay, Antananarivo,
+            révélation fintech de women in Africa, 2020. 
+          </p>
+
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-3">
+        <div class="five">
+          <img src="../floix_template/img-prof/Harry.jpg" alt="">
+          <h5>HARRY</h5>
+          <p>enseignant du module SYS1, Chef de projet digitale chez Baobab Group.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="parcours">
+    <br><h2>PARCOURS PROPOSE</h2>
+    <p> Nous proposons deux parcours distincts: <b class="gras">Ecosystèmes Logiciels (EL)</b> et <b class="gras">Transformations Numériques (TN)</b> sanctionnés par le diplôme de grade Licence.
+      Notre formation se base sur deux méthodes complémentaires: </p>
+    <ul>
+      <li><b class="gras">. Modules techniques en informatiques</b> selon le parcours choisi: Les cours théoriques à distance (25%), suivi de vidéos + cours en présentiel (25%) de 2h </li>
+      <li><b class="gras">. Apprentissage en entreprise</b> (50%): application pratique suivant un contrat d'alternance</li>
+    </ul>
+      
+    <div>
+      <img src="../floix_template/img-prof/Place.jpg" alt="">
+    </div>
+  </div>
+
+  <div>
+    <div class="partenaire">
+      <div class="row">
+        <div class="col-md-4 col-sm-4">
+          <h2>Saviez-Vous Que??</h2>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <img src="../floix_template/img-prof/minion.gif" alt="">
+        </div>
+        <div class="col-md-5 col-sm-5">
+          <p><b>HEI</b> travaille en étroite collaboration avec l'<b>Alliance Française</b> notamment pour le perfectionnement en français professionnel.<br>
+            Actuellement, 5 entreprises française sont prêtes à accueillir les étudiants en alternance à partir de la 2ème année. Ce partenariat leur permettra de ne pas payer de frais de scolarités.<br>
+            "La communication est aussi importante que la technique": l'<b>Alliance française</b> assure les cours de français d'<b>HEI</b>.
+          </p>
+          <a class="logoAF" href="https://www.facebook.com/Afantananarivo/"><img src="../floix_template/img-prof/alliance-francaise-antananarivo-logo.jpg" alt=""></a><br>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="frais">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <img src="../floix_template/img-prof/Paris.jpg" alt="">
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <br><h4>Mensualité et Bourses d'études</h4>
+        <div>
+          <p>Les frais de scolarités sont de 1.600.000 Ar si payé en une seul fois, à la rentrée et 1.800.000 Ar payable sur 9 mois.</p>
+        </div>
+        <div>
+          <p>Alignée avec ses valeurs d'entraide et d'autonomie, <b>HEI</b> va octroyer une Bourse d'excellence à hauteur de 8 900 € soit 40.000.000 Ar pour le/la major de promotion afin de poursuivre ses études en France.</p>
+        </div>
+        <div>
+          <p>De plus, seront exonérés de frais de scolarités de la 1ère année d'études les 3 étudiants les mieux classés au concours d'entrée et 2 étudiants suivant des critères sociaux.</p><br>
+        </div>
+        <div>
+          <a href="https://heischool.wixsite.com/vatsy" class="vatsy"><b>Programme VATSY</b></a>
+        </div>
+      </div>
+    </div>
+  </div><br>
+
+
+
+<!-- work section -->
+
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/1.jpg" class="work-box"> <img src="../floix_template/img-prof/1.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/2.jpg" class="work-box"> <img src="../floix_template/img-prof/2.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/3.jpg" class="work-box"> <img src="../floix_template/img-prof/3.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/4.jpg" class="work-box"> <img src="../floix_template/img-prof/4.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/5.jpg" class="work-box"> <img src="../floix_template/img-prof/5.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/6.jpg" class="work-box"> <img src="../floix_template/img-prof/6.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/7.jpg" class="work-box"> <img src="../floix_template/img-prof/7.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+      <div class="col-lg-3 col-md-5 col-sm-5 work"> <a href="../floix_template/img-prof/8.jpg" class="work-box"> <img src="../floix_template/img-prof/8.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- work section --> 
+<!-- concours -->
+<section id="concours" class="milieu">
+  <div class="cours">
+    <h3><b>Inscription au concours d'entrée L1</b></h3>
+    <p>Téléchargez <a class="fiche" href="https://258d9e3d-3d80-456f-a3a8-d7f87ac4fe40.filesusr.com/ugd/c6fa1c_7211b91258ef49cd963a2d7c39a3fd59.pdf"><u> la fiche de renseignement </u></a><br>
+      Payez 40000 Ar par MVola au 034 94 041 16 <b>(Fihariantsoa)</b> et notez sa référence.<br>
+      Remplissez les champs ci-dessous : </p>
+
+    <form action="validation.php" class="champ">
+      <input type="text" value="" class="form-control" id="validationDefault01" placeholder="Prénom" required><br>
+      <input type="text" value="" class="form-control" id="validationDefault02" placeholder="Nom de famille" required><br>
+      <input type="text" value="" class="form-control" id="validationDefault03" placeholder="E-mail" required><br>
+      <input type="number" value="" class="form-control" id="validationDefault04" placeholder="Téléphone" required>
+      <div class="rien">  
+        <div class="rien1">
+          <p>CIN ou Acte de naissance  <span class="fat"> + </span> </p></div>
+          <p>Photo d'identité récente  <span class="fat"> + </span> </p>
+          <p>Fiche de renseignement remplie  <span class="fat"> + </span> </p>
+
+        </div>
+      <input type="number" value="" class="form-control" id="validationDefault05" placeholder="N° référence MVola" required>
+      <div class="ecrire">
+        <p>Date de concours souhaitée: </p>
+        <div class="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="customRadioInline1" name="customRadioInline" class="custom-control-input">
+          <label class="custom-control-label" for="customRadioInline1"> 15 mars 2022</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="customRadioInline2" name="customRadioInline" class="custom-control-input">
+          <label class="custom-control-label" for="customRadioInline2"> 16 mars 2022</label>
+        </div>
+      </div>
+      <div class="rien">
+        <p>Bulletins de la terminale ou relevé de note BAC <span class="fat"> + </span> </p>
+      </div>
+
+      <button class="btn btn-primary" type="submit">S'inscrire</button>
+    </form>
+  </div>
+    
+</section>
+
+<!-- footer section -->
+<footer id="contact" class="footer">
+  <div class="container">
+    <div class="col-md-4">
+      <h4><b>Contact</b></h4>
+      <p> Lot II J 161 Analamahitsy Ivandry - Antananarivo<br>
+        Call: 034 94 041 16 / 032 71 179 48 <br>
+        Email : <a href="contact@hei.school"> contact@hei.school </a><br>
+        Facebook : <a href="https://www.facebook.com/hei.school/"> HEI </a></p>
+    </div>
+    <div class="col-md-5">
+      <h4>Social network</h4>
+      <ul class="footer-share">
+        <li><a href="https://www.facebook.com/hei.school/"><img class="plus" src="../floix template/img-prof/facebook.png"></a></li>
+        <li><a href="contact@hei.school"><img class="plus" src="../floix template/img-prof/gmail.png"></a></li>
+        <li><a href="https://www.linkedin.com/company/hei-school/"><img class="moins" src="../floix template/img-prof/Linkedin.jpg"></a></li>
+      </ul>
+    </div>
+    <div class="col-md-3">
+      <div class="fin">
+        <p>© 2020 - 2021 HEI <br>
+          SARLU basée à Antananarivo, Madagascar<br>
+        au capital de 40,000,000 Ar soit environ 10,000 €</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- footer section --> 
+
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/jquery.fancybox.pack.js"></script> 
+<script src="js/retina.min.js"></script> 
+<script src="js/modernizr.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
